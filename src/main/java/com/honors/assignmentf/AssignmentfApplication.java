59aa111c43f8a50c6ab47d1057922609d09680f6
yashtapre77 <yashtapre77@gmail.com>
2025-03-18 06:58:26 +0530

    Project Initialization

---
+++ src/main/java/com/honors/assignmentf/AssignmentfApplication.java
@@ -0,0 +1,13 @@
+package com.honors.assignmentf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/honors/assignmentf/AssignmentfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honors.assignmentf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
