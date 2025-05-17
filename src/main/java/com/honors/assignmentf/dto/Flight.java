ef434939b5748219e28bdbca08dea2d31c8e4811
yashtapre77 <yashtapre77@gmail.com>
2025-05-17 07:17:23 +0530

    updated java version

--- src/main/java/com/honors/assignmentf/dto/Flight.java
+++ src/main/java/com/honors/assignmentf/dto/Flight.java
@@ -3,12 +3,14 @@ package com.honors.assignmentf.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder(toBuilder = true)
 @AllArgsConstructor
+@Getter
 public class Flight {
     private String id;
     private String source;
--- src/main/java/com/honors/assignmentf/dto/Ticket.java
+++ src/main/java/com/honors/assignmentf/dto/Ticket.java
@@ -3,10 +3,12 @@ package com.honors.assignmentf.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 @Builder(toBuilder = true)
 @AllArgsConstructor
+@Getter
 public class Ticket {
 
     private String id;
